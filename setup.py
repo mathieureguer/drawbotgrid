1b2822ed7abd900bcc32a79004e0041c53132c57
Mathieu Reguer <mathieu.reguer@gmail.com>
2022-09-28 15:11:45 +0200

    Improved directeory structure

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+# def readme():
+#     with open("README.md") as f:
+#         return f.read()
+
+setup(name="DrawBotGrid",
+      version="0.1",
+      description="a little helper for grid based layout in DrawBot",
+      long_description="TBD",
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: Other/Proprietary License",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Build Tools",
+      ],
+      author="Mathieu Reguer",
+      author_email="mathieu.reguer@gmail.com",
+      license="All rights reserved",
+      packages=[
+        "DrawBotGrid",
+        ],
+      install_requires=[
+        #"drawBot",
+
+      ],
+      include_package_data=True,
+      zip_safe=False)
